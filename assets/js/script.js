d999af673d6bab8c3e5fddebe546b2df62cb4ca7
Nick Rizzo <nickrizzo77277@gmail.com>
2021-11-09 10:55:38 -0600

    set up local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,54 @@
 //Display current data and time
 $("#currentDay").text(moment().format("HH:mm, MMMM Do YYYY"));
 
-function timeTracker() {
-  var currentTime = moment().hour();
+$(document).ready(function () {
+  //save btn to local storage
 
-  if (rowTime < currentTime) {
-    $(this).removeClass("present");
-    $(this).removeClass("future");
-    $(this).addClass("past");
-  } else if (currentTime === rowTime) {
-    $(this).removeClass("past");
-    $(this).removeClass("future");
-    $(this).addClass("present");
-  } else {
-    $(this).removeClass("past");
-    $(this).removeClass("present");
-    $(this).addClass("future");
+  $(".saveBtn").on("click", function () {
+    // get values
+    var textValue = $(this).siblings(".description").val();
+    var timeAdded = $(this).parent().attr("id");
+    console.log(textValue);
+    console.log(timeAdded);
+    // save the value in localStorage
+    localStorage.setItem(timeAdded, textValue);
+    JSON.stringify(timeAdded, textValue);
+  });
+
+  function timeTracker() {
+    var currentTime = moment().hour();
+
+    $(".time-block").each(function () {
+      var rowTime = parseInt($(this).attr("id").split("-")[1]);
+
+      if (rowTime < currentTime) {
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+        $(this).addClass("past");
+      } else if (currentTime === rowTime) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+
+        //Retrieving from local storage
+
+        $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+        $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+        $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+        $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+        $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+        $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+        $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+        $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+        $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+        $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+      }
+    });
   }
-}
 
-timeTracker();
+  timeTracker();
+});
